2182d5794118bd3eb2e3026ef15ce7c03c600edc
Mike McDonald <whoiswizard@icloud.com>
2021-04-09 01:08:59 -0500

    finished layout/style of navbar dropdown

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -2,16 +2,18 @@ import React from "react";
 import "../App.css";
 
 const Instructions = (props) => (
-   <section className="section">
+   <section className="section ">
        <div className="container ">
        <div className="columns is-vcentered">
            <div className="column">
-           <div className="box has-text-justified">
-           <h2 className="title">Um, is this the boring, peaceful kind of taking to the streets?</h2>
-<p>Shut up and get to the point! What's with you kids? Every other day it's food, food, food. Alright, I'll get you some stupid food. I'm Santa Claus! Spare me your space age technobabble, Attila the Hun!</p>
+           <div className="box d-shadow ">
+           <div className="content">
+           <h2 className="title">Interesting. No, wait, the other thing: tedious.</h2>
+<p>Not tricks, Michael, illusions. No, I did not kill Kitty. However, I am going to oblige and answer the nice officer's questions because I am an honest man with no secrets to hide. Say goodbye to these, because it's the last time!</p>
 <ol>
 <li>But existing is basically all I do!</li><li>Oh God, what have I done?</li><li>Please, Don-Bot… look into your hard drive, and open your mercy file!</li>
 </ol>
+</div>
   </div>
            </div>
            <div className="column">
